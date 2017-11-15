5825f7897564d2373128dad862f077cef147ca49
saryDong <2997723775@qq.com>
2017-11-15 21:11:28 +0800

    咨询初步优化

---
+++ app/src/main/java/com/cxjd/nvwabao/bean/TitleBean.java
@@ -0,0 +1,57 @@
+package com.cxjd.nvwabao.bean;
+
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by abu on 2017/11/15.
+ */
+
+public class TitleBean extends DataSupport {
+    private String titleName;
+    private String titleCategery;
+    private int title_check;
+    private String titleLable;
+
+    public String getTitleName() {
+        return titleName;
+    }
+
+    public void setTitleName(String titleName) {
+        this.titleName = titleName;
+    }
+
+    public String getTitleCategery() {
+        return titleCategery;
+    }
+
+    public void setTitleCategery(String titleCategery) {
+        this.titleCategery = titleCategery;
+    }
+
+    public int getTitle_check() {
+        return title_check;
+    }
+
+    public void setTitle_check(int title_check) {
+        this.title_check = title_check;
+    }
+
+    public String getTitleLable() {
+        return titleLable;
+    }
+
+    public void setTitleLable(String titleLable) {
+        this.titleLable = titleLable;
+    }
+
+    public TitleBean(String titleName, String titleCategery, int title_check, String titleLable) {
+        this.titleName = titleName;
+        this.titleCategery = titleCategery;
+        this.title_check = title_check;
+        this.titleLable = titleLable;
+    }
+
+    public TitleBean() {
+    }
+}
