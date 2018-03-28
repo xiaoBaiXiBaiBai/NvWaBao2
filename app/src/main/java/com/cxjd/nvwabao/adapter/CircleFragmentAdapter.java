c739ef1214b5bf89f155e5ae7e2b62ff497bf556
千夜 <1342746626@qq.com>
2018-03-28 20:49:26 +0800

    适配器完善

---
+++ app/src/main/java/com/cxjd/nvwabao/adapter/CircleFragmentAdapter.java
@@ -0,0 +1,152 @@
+package com.cxjd.nvwabao.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cxjd.nvwabao.Activity.PeopleChatActivity;
+import com.cxjd.nvwabao.Activity.PeopleInfoActivity;
+import com.cxjd.nvwabao.R;
+import com.cxjd.nvwabao.bean.PeopleChat;
+
+import java.util.List;
+
+/**
+ * Created by 白 on 2018/3/26.
+ * <p>
+ * 圈子中 动态的 适配器
+ */
+
+public class CircleFragmentAdapter extends ArrayAdapter<PeopleChat>  {
+
+    private int resourceId;
+    private List<PeopleChat> peopleChatList;
+
+
+    private Context mContext;
+
+    private int positionBB;   //单行序列数
+
+    public CircleFragmentAdapter(@NonNull Context context, int textViewResourceId, @NonNull List<PeopleChat> objects) {
+        //初始化数据
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+        peopleChatList = objects;
+        mContext = context;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view;
+        //实例ViewHolder，当程序第一次运行，保存获取到的控件，提高效率
+        ViewHolder viewHolder;
+
+        if(convertView==null){
+            viewHolder=new ViewHolder();
+            view = LayoutInflater.from(getContext()).inflate(resourceId, null);
+            viewHolder.image_people = (ImageView) view.findViewById(R.id.image_people);//获取控件,只需要调用一遍，调用过后保存在ViewHolder中
+            viewHolder.name_people = (TextView) view.findViewById(R.id.name_people);   //获取控件
+            viewHolder.chat_people = (TextView) view.findViewById(R.id.chat_people);
+            viewHolder.go_people = (ImageView) view.findViewById(R.id.go_people);
+
+            view.setTag(viewHolder);
+        }else{
+            view=convertView;           //convertView不为空代表布局被加载过，只需要将convertView的值取出即可
+            viewHolder=(ViewHolder) view.getTag();
+        }
+
+        PeopleChat peopleChat = getItem(position);
+
+        viewHolder.image_people.setImageResource(peopleChat.getImageId());
+        viewHolder.name_people.setText(peopleChat.getName());
+        viewHolder.chat_people.setText(peopleChat.getChat());
+
+        //positionBB = position;
+
+        viewHolder.image_people.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                PeopleChat peopleChat = peopleChatList.get(position);
+                Intent intent = new Intent(mContext, PeopleInfoActivity.class);
+                Log.i("i", "onClick: 传递的id为+++++"+peopleChat.getPeopleId());
+                bundle.putInt("id", peopleChat.getPeopleId());
+                intent.putExtras(bundle);
+                mContext.startActivity(intent);
+            }
+        });
+        viewHolder.chat_people.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                PeopleChat peopleChat = peopleChatList.get(position);
+                Intent intent = new Intent(mContext, PeopleChatActivity.class);
+                bundle.putInt("id", peopleChat.getPeopleId());
+                intent.putExtras(bundle);
+                mContext.startActivity(intent);
+            }
+        });
+       viewHolder.go_people.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Bundle bundle = new Bundle();
+               PeopleChat peopleChat = peopleChatList.get(position);
+               Intent intent = new Intent(mContext, PeopleChatActivity.class);
+               bundle.putInt("id", peopleChat.getPeopleId());
+               intent.putExtras(bundle);
+               mContext.startActivity(intent);
+           }
+       });
+
+
+        System.out.println("进入遍历------------------------"+peopleChat.getName());
+
+        return view;
+
+    }
+
+/*    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.image_people:
+                Bundle bundle2 = new Bundle();
+                PeopleChat peopleChat2 = peopleChatList.get(positionBB);
+                Intent intent2 = new Intent(mContext, PeopleInfoActivity.class);
+                bundle2.putInt("id", peopleChat2.getPeopleId());
+                intent2.putExtras(bundle2);
+                mContext.startActivity(intent2);
+                break;
+            case R.id.chat_people:
+                Bundle bundle = new Bundle();
+                PeopleChat peopleChat = peopleChatList.get(positionBB);
+                Intent intent = new Intent(mContext, PeopleChatActivity.class);
+                bundle.putInt("id", peopleChat.getPeopleId());
+                intent.putExtras(bundle);
+                mContext.startActivity(intent);
+                break;
+            case R.id.go_people:
+
+                break;
+
+        }
+
+    }*/
+
+    class ViewHolder{ //当布局加载过后，保存获取到的控件信息
+        ImageView image_people;
+        TextView name_people;
+        TextView chat_people;
+        ImageView go_people;
+    }
+
+}
--- app/src/main/java/com/cxjd/nvwabao/adapter/PeopleChatAdapter.java
+++ app/src/main/java/com/cxjd/nvwabao/adapter/PeopleChatAdapter.java
@@ -12,7 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cxjd.nvwabao.R;
-import com.cxjd.nvwabao.bean.PeopleChat;
+import com.cxjd.nvwabao.bean.PeopleReturn;
 
 import java.util.List;
 
@@ -21,13 +21,13 @@ import java.util.List;
  * 圈子聊天列表适配器
  */
 
-public class PeopleChatAdapter extends ArrayAdapter<PeopleChat> {
+public class PeopleChatAdapter extends ArrayAdapter<PeopleReturn> {
 
     private int resourceId;
 
 
 
-    public PeopleChatAdapter(Context context,int textViewResourceId, List<PeopleChat> objects) {
+    public PeopleChatAdapter(Context context,int textViewResourceId, List<PeopleReturn> objects) {
         super(context,textViewResourceId, objects);
 
         resourceId = textViewResourceId;//获取子布局
@@ -45,26 +45,30 @@ public class PeopleChatAdapter extends ArrayAdapter<PeopleChat> {
             view = LayoutInflater.from(getContext()).inflate(resourceId, null);
             viewHolder.ivImage = (ImageView) view.findViewById(R.id.ivImage);//获取控件,只需要调用一遍，调用过后保存在ViewHolder中
             viewHolder.tvName = (TextView) view.findViewById(R.id.tvName);   //获取控件
+            viewHolder.chat_dec = (TextView) view.findViewById(R.id.chat_dec);
             view.setTag(viewHolder);
         }else{
             view=convertView;           //convertView不为空代表布局被加载过，只需要将convertView的值取出即可
             viewHolder=(ViewHolder) view.getTag();
         }
 
-        PeopleChat peopleChat = getItem(position);
+        PeopleReturn peopleReturn = getItem(position);
 
-        viewHolder.ivImage.setImageResource(peopleChat.getImageId());
-        viewHolder.tvName.setText(peopleChat.getChat());
-
-        System.out.println("尝试获取text"+peopleChat.getChat());
+        viewHolder.ivImage.setImageResource(peopleReturn.getImageId());
+        viewHolder.tvName.setText(peopleReturn.getReturnName());
+        viewHolder.chat_dec.setText(peopleReturn.getReturnChat());
 
         return view;
     }
 
+
+
+    class ViewHolder{ //当布局加载过后，保存获取到的控件信息
+        ImageView ivImage;
+        TextView tvName;
+        TextView chat_dec;
+
+    }
+
 }
 
-class ViewHolder{ //当布局加载过后，保存获取到的控件信息
-    ImageView ivImage;
-    TextView tvName;
-
-}
